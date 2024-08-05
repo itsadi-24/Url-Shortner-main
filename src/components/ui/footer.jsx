229b795ec267152d2f2a71d757e0bb22bc406b5d
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-08-05 23:32:51 +0530

    "Updated Footer component with new styles, added links to GitHub and LinkedIn, and changed personal website link."

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -1,19 +1,42 @@
 export default function Footer() {
   return (
-    <div className='text-center '>
-      <div className='inline-block w-full px-6 py-3 bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-10 '>
-        <a
-          href='https://www.linkedin.com/in/adi-prasan-khuntia-3944072a5/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='relative font-sans text-lg text-white transition-all duration-300 ease-in-out cursor-pointer hover:text-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50 group'
-        >
-          <span className='font-extrabold tracking-wide'>
-            Made with <span className='text-red-500'>💓</span> by Adi
-          </span>
-          <span className='absolute left-0 bottom-0 w-full h-0.5 bg-purple-300 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
-        </a>
+    <footer className='py-3 bg-gradient-to-r from-gray-800 via-gray-900 to-black'>
+      <div className='container px-6 mx-auto text-center'>
+        <div className='relative inline-block px-6 py-3 bg-white rounded-lg shadow-lg bg-opacity-10 backdrop-filter backdrop-blur-lg'>
+          <a
+            href='https://www.adiprasan.me/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='relative font-sans text-lg text-white transition-all duration-300 ease-in-out cursor-pointer hover:text-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50 group'
+          >
+            <span className='font-extrabold tracking-wide'>
+              Made with <span className='text-red-500'>💓</span> by Adi
+            </span>
+            <span className='absolute inset-x-0 bottom-0 h-0.5 bg-purple-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </a>
+        </div>
+        <p>
+          Connect with me on{' '}
+          <a
+            href='https://github.com/itsadi-24'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-white transition duration-300 hover:text-purple-400 hover:underline '
+          >
+            GitHub
+          </a>{' '}
+          and{' '}
+          <a
+            href='https://www.linkedin.com/in/adi-prasan-khuntia-3944072a5/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-white transition duration-300 hover:text-purple-400 hover:underline'
+          >
+            LinkedIn
+          </a>
+          .
+        </p>
       </div>
-    </div>
+    </footer>
   );
 }
