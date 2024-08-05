590f7e8dd8282adeaecd0bc5e1c4ba31164c8a9d
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-08-05 23:43:19 +0530

    "Update footer background gradient colors"

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -6,7 +6,15 @@ import useFetch from '@/hooks/use-fetch';
 import { deleteUrl, getUrl } from '@/db/apiUrls';
 import { UrlState } from '@/context';
 import { getClicksForUrl } from '@/db/apiClicks';
-import { Copy, Download, LinkIcon, Trash, CheckCircle } from 'lucide-react';
+import {
+  Copy,
+  Download,
+  LinkIcon,
+  Trash,
+  CheckCircle,
+  Globe,
+  Smartphone,
+} from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import DeviceStats from '../components/ui/device-stats';
 import Location from '../components/ui/location-stats';
@@ -23,13 +31,11 @@ const Link = () => {
     fn,
     error,
   } = useFetch(getUrl, { id, user_id: user?.id });
-
   const {
     loading: loadingStats,
     data: stats,
     fn: fnStats,
   } = useFetch(getClicksForUrl, id);
-
   const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
 
   useEffect(() => {
@@ -68,44 +74,42 @@ const Link = () => {
   const handleCopy = () => {
     navigator.clipboard.writeText(newLink);
     setCopied(true);
-
     setTimeout(() => setCopied(false), 2000);
   };
 
   return (
-    <div className='min-h-screen p-8 text-white bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-800'>
+    <div className='min-h-screen p-8 text-white bg-gradient-to-br from-gray-900 via-purple-900 to-violet-900'>
       {(loading || loadingStats) && (
-        <BarLoader width={'100%'} color='#f472b6' />
+        <BarLoader width={'100%'} color='#8b5cf6' />
       )}
       <div className='mx-auto max-w-7xl'>
-        <h1 className='mb-12 text-5xl font-extrabold tracking-tight text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-purple-300'>
+        <h1 className='mb-12 text-5xl font-extrabold tracking-tight text-center text-transparent bg-clip-text bg-gradient-to-r from-violet-300 to-purple-300'>
           Link Details
         </h1>
         <div className='grid grid-cols-1 gap-8 lg:grid-cols-3'>
           <div className='col-span-1'>
-            <Card className='h-full overflow-hidden bg-white border-0 shadow-xl bg-opacity-10 backdrop-filter backdrop-blur-lg'>
-              <CardHeader className='bg-gradient-to-r from-pink-500 to-purple-500'>
+            <Card className='h-full overflow-hidden bg-gray-800 border-0 shadow-xl'>
+              <CardHeader className='bg-gradient-to-r from-violet-600 to-purple-600'>
                 <CardTitle className='text-2xl font-bold text-white'>
                   {url?.title}
                 </CardTitle>
               </CardHeader>
               <CardContent className='p-6 space-y-6'>
-                <div className='relative group'>
+                <div className='p-4 bg-gray-700 rounded-lg'>
                   <a
                     href={newLink}
                     target='_blank'
                     rel='noopener noreferrer'
-                    className='text-xl font-semibold text-blue-300 break-all transition duration-300 hover:text-blue-200'
+                    className='text-xl font-semibold break-all transition duration-300 text-violet-300 hover:text-violet-200'
                   >
                     {newLink}
                   </a>
-                  <div className='absolute inset-0 transition-opacity duration-300 bg-blue-500 rounded opacity-0 group-hover:opacity-10'></div>
                 </div>
                 <a
                   href={url?.original_url}
                   target='_blank'
                   rel='noopener noreferrer'
-                  className='flex items-center gap-2 text-gray-300 transition duration-300 hover:text-white'
+                  className='flex items-center gap-2 p-2 text-gray-300 transition duration-300 rounded-md hover:text-white hover:bg-gray-700'
                 >
                   <LinkIcon size={16} />
                   <span className='break-all'>{url?.original_url}</span>
@@ -118,8 +122,8 @@ const Link = () => {
                     variant='outline'
                     size='icon'
                     onClick={handleCopy}
-                    className={`bg-pink-500 hover:bg-pink-600 text-white border-0 transition duration-300 ${
-                      copied ? 'bg-green-500 hover:bg-green-600' : ''
+                    className={`bg-violet-600 hover:bg-violet-700 text-white border-0 transition duration-300 ${
+                      copied ? 'bg-green-600 hover:bg-green-700' : ''
                     }`}
                   >
                     {copied ? <CheckCircle size={16} /> : <Copy size={16} />}
@@ -128,7 +132,7 @@ const Link = () => {
                     variant='outline'
                     size='icon'
                     onClick={downloadImage}
-                    className='text-white transition duration-300 bg-purple-500 border-0 hover:bg-purple-600'
+                    className='text-white transition duration-300 bg-purple-600 border-0 hover:bg-purple-700'
                   >
                     <Download size={16} />
                   </Button>
@@ -136,12 +140,10 @@ const Link = () => {
                     variant='outline'
                     size='icon'
                     onClick={() =>
-                      fnDelete().then(() => {
-                        navigate('/dashboard');
-                      })
+                      fnDelete().then(() => navigate('/dashboard'))
                     }
                     disabled={loadingDelete}
-                    className='text-white transition duration-300 bg-red-500 border-0 hover:bg-red-600'
+                    className='text-white transition duration-300 bg-red-600 border-0 hover:bg-red-700'
                   >
                     {loadingDelete ? (
                       <BeatLoader size={5} color='white' />
@@ -150,7 +152,7 @@ const Link = () => {
                     )}
                   </Button>
                 </div>
-                <div className='p-4 mt-6 bg-white rounded-lg bg-opacity-20'>
+                <div className='p-4 mt-6 bg-white rounded-lg'>
                   <img
                     src={url?.qr}
                     alt='QR code'
@@ -161,8 +163,8 @@ const Link = () => {
             </Card>
           </div>
           <div className='lg:col-span-2'>
-            <Card className='h-full overflow-hidden bg-white border-0 shadow-xl bg-opacity-10 backdrop-filter backdrop-blur-lg'>
-              <CardHeader className='bg-gradient-to-r from-purple-500 to-pink-500'>
+            <Card className='h-full overflow-hidden bg-gray-800 border-0 shadow-xl'>
+              <CardHeader className='bg-gradient-to-r from-purple-600 to-violet-600'>
                 <CardTitle className='text-2xl font-bold text-white'>
                   Statistics
                 </CardTitle>
@@ -170,7 +172,7 @@ const Link = () => {
               <CardContent className='p-6'>
                 {stats && stats.length ? (
                   <div className='space-y-8'>
-                    <div className='p-6 text-center rounded-lg shadow-lg bg-gradient-to-r from-pink-500 to-purple-500'>
+                    <div className='p-6 text-center rounded-lg shadow-lg bg-gradient-to-r from-violet-600 to-purple-600'>
                       <h3 className='mb-2 text-xl font-semibold text-white'>
                         Total Clicks
                       </h3>
@@ -179,30 +181,28 @@ const Link = () => {
                       </p>
                     </div>
                     <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
-                      <div>
-                        <Card className='overflow-hidden bg-white border-0 shadow-lg bg-opacity-20'>
-                          <CardHeader className='bg-gradient-to-r from-blue-500 to-cyan-500'>
-                            <CardTitle className='text-lg font-semibold text-white'>
-                              Location Stats
-                            </CardTitle>
-                          </CardHeader>
-                          <CardContent className='p-4'>
-                            <Location stats={stats} />
-                          </CardContent>
-                        </Card>
-                      </div>
-                      <div>
-                        <Card className='overflow-hidden bg-white border-0 shadow-lg bg-opacity-20'>
-                          <CardHeader className='bg-gradient-to-r from-green-500 to-teal-500'>
-                            <CardTitle className='text-lg font-semibold text-white'>
-                              Device Stats
-                            </CardTitle>
-                          </CardHeader>
-                          <CardContent className='p-4'>
-                            <DeviceStats stats={stats} />
-                          </CardContent>
-                        </Card>
-                      </div>
+                      <Card className='overflow-hidden bg-gray-700 border-0 shadow-lg'>
+                        <CardHeader className='bg-gradient-to-r from-blue-600 to-cyan-600'>
+                          <CardTitle className='flex items-center text-lg font-semibold text-white'>
+                            <Globe className='mr-2' size={20} />
+                            Location Stats
+                          </CardTitle>
+                        </CardHeader>
+                        <CardContent className='p-4'>
+                          <Location stats={stats} />
+                        </CardContent>
+                      </Card>
+                      <Card className='overflow-hidden bg-gray-700 border-0 shadow-lg'>
+                        <CardHeader className='bg-gradient-to-r from-green-600 to-teal-600'>
+                          <CardTitle className='flex items-center text-lg font-semibold text-white'>
+                            <Smartphone className='mr-2' size={20} />
+                            Device Stats
+                          </CardTitle>
+                        </CardHeader>
+                        <CardContent className='p-4'>
+                          <DeviceStats stats={stats} />
+                        </CardContent>
+                      </Card>
                     </div>
                   </div>
                 ) : (
