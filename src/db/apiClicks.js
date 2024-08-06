a56a2ed750817537be8bfc612cd611cf72c8ac8b
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-08-06 14:44:53 +0530

    "Updated index.html title, removed public/banner.png and public/logo.jpg, refactored src/db/apiAuth.js and src/db/apiClicks.js"

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,55 +1,52 @@
+import supabase from './supabase';
+import { UAParser } from 'ua-parser-js';
 
-import supabase from "./supabase";
-import {UAParser} from "ua-parser-js";
+export async function getClicksForUrls(urlIds) {
+  const { data, error } = await supabase
+    .from('clicks')
+    .select('*')
+    .in('url_id', urlIds);
 
+  if (error) {
+    console.error('Error fetching clicks:', error);
+    return null;
+  }
 
-      export async function getClicksForUrls(urlIds) {
-        const {data, error} = await supabase
-          .from("clicks")
-          .select("*")
-          .in("url_id", urlIds);
-      
-        if (error) {
-          console.error("Error fetching clicks:", error);
-          return null;
-        }
-      
-        return data;
-      }
-      
-      export async function getClicksForUrl(url_id) {
-        const {data, error} = await supabase
-          .from("clicks")
-          .select("*")
-          .eq("url_id", url_id);
-      
-        if (error) {
-          console.error(error);
-          throw new Error("Unable to load Stats");
-        }
-      
-        return data;
-      }
+  return data;
+}
 
-      const parser = new UAParser();
+export async function getClicksForUrl(url_id) {
+  const { data, error } = await supabase
+    .from('clicks')
+    .select('*')
+    .eq('url_id', url_id);
 
-      export const storeClicks = async({id, originalUrl}) => {
-        try{
-          const res= parser.getResult();
-          const device= res.type || "desktop";
+  if (error) {
+    console.error(error);
+    throw new Error('Unable to load Stats');
+  }
 
-          const response = await fetch("https://ipapi.co/json");
-          const {city, country_name: country}= await response.json();
+  return data;
+}
 
-          await supabase.from("clicks").insert({
-            url_id: id,
-            city: city,
-            country: country,
-            device: device,
-          })
-          window.location.href = originalUrl;
+const parser = new UAParser();
 
-        }catch(error){
-           console.error("Error recording click:", error);
-        }
-      } 
+export const storeClicks = async ({ id, originalUrl }) => {
+  try {
+    const res = parser.getResult();
+    const device = res.type || 'desktop';
+
+    const response = await fetch('https://ipapi.co/json');
+    const { city, country_name: country } = await response.json();
+
+    await supabase.from('clicks').insert({
+      url_id: id,
+      city: city,
+      country: country,
+      device: device,
+    });
+    window.location.href = originalUrl;
+  } catch (error) {
+    console.error('Error recording click:', error);
+  }
+};
